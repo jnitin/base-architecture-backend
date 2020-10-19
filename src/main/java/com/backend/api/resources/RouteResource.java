992b70b3604dd073a6ca26c0d4e08d922621b453
Gabriel <gabriel.costa@uds.com.br>
2020-10-19 11:51:23 -0400

    Correções de imports desnecessários;

--- src/main/java/com/backend/api/resources/RouteResource.java
+++ src/main/java/com/backend/api/resources/RouteResource.java
@@ -1,6 +1,5 @@
 package com.backend.api.resources;
 
-import java.net.URI;
 import java.util.List;
 
 import com.backend.api.domain.Route;
@@ -11,7 +10,6 @@ import com.backend.api.services.RouteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/routes")
